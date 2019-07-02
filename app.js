2398a1a88c446b5d0ea59db9db9be55b35fd9407
ducthangwru <thangnd520@gmail.com>
2019-07-02 22:42:09 +0700

    update

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 
 const app = express()
-var server = require('http').createServer(app);
-const wsServer = new WebSocket.Server({ server: server }, () => console.log(`WS server is listening at ws://localhost:${process.env.PORT}`));
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
 
 // Body parser middleware
 app.use(bodyParser.json())
@@ -24,25 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/api', publicController);
 // app.use('/api/user', usersController);
-
-let connectedClients = [];
-
-wsServer.on('connection', (ws, req) => {
-    console.log('Connected');
-    // add new connected client
-    connectedClients.push(ws);
-    // listen for messages from the streamer, the clients will not send anything so we don't need to filter
-    ws.on('message', data => {
-        // send the base64 encoded frame to each connected ws
-        connectedClients.forEach((ws, i) => {
-            if (ws.readyState === ws.OPEN) { // check if it is still connected
-                ws.send(data); // send
-            } else { // if it's not connected remove from the array of connected ws
-                connectedClients.splice(i, 1);
-            }
-        });
-    });
-});
 
 // HTTP stuff
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './index.html')));
