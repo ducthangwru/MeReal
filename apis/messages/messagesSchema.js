cfe62ff049ba0db6a4b2ae3556d1b5dfaec9e7df
ducthangwru <ducthangwru97@gmail.com>
2019-07-03 16:59:01 +0700

    update message schema

---
+++ apis/messages/messagesSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const messagesSchema = new Schema(
+    {
+        user_from : {type : ObjectId, require : true, ref : 'users'},
+        user_to : {type : ObjectId, require : true, ref : 'users'},
+        content : {type : String, require : true}
+    }, {timestamps : {createAt : 'created_at', updateAt : 'updated_at'}}
+)
+
+module.exports = messagesSchema
