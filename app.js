8b6550746dc771277a842240235934bc4932e057
ducthangwru <thangnd520@gmail.com>
2019-07-03 22:38:17 +0700

    update pusher

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const Pusher = require('pusher');
 const PORT = process.env.PORT || 3000
 const app = express()
 var server = require('http').createServer(app)
@@ -25,14 +26,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/api', publicController);
 // app.use('/api/user', usersController);
 
-// HTTP stuff
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './index.html')));
+// Create an instance of Pusher
+const pusher = new Pusher({
+  appId: '810804',
+  key: 'f70f1870659a69d4de88',
+  secret: '4f09e3455f0386b4eac7',
+  cluster: 'ap1',
+  encrypted: true
+})
+
+app.get('/', (req, res) => {
+  return res.sendFile(__dirname + '/index.html')
+})
+
+// get authentictation for the channel;
+app.post('/pusher/auth', (req, res) => {
+      const socketId = req.body.socket_id;
+      const channel = req.body.channel_name;
+      var presenceData = {
+          user_id: Math.random().toString(36).slice(2) + Date.now()
+      }
+      const auth = pusher.authenticate(socketId, channel, presenceData)
+      res.send(auth)
+})
 
 mongoose.connect(CONNECT_MONGO, (err) => {
     if (err) {
       console.log(err);
     } else {
-      console.log('Connect to db success');
+      console.log('Connect to db success')
     }
 })
 
